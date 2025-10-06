f55b61389840f41b9aa30097b5ff09ce02c706de
rchini26 <rafa.montm@gmail.com>
2025-10-06 14:11:07 -0300

    Added Run Speed

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,7 +7,9 @@ public class PlayerController : MonoBehaviour
    public Rigidbody2D rb;
    public Vector2 friction = new Vector2(-.1f, 0);
    public float speed;
+   public float runSpeed;
    public float jumpForce = 15;
+   private float _currentSpeed;
    private void Update()
    {
       HandleJump();
@@ -16,15 +18,32 @@ public class PlayerController : MonoBehaviour
 
    void HandleMovement()
    {
+      if(Input.GetKey(KeyCode.LeftShift))
+      {
+         _currentSpeed = runSpeed;
+      }
+      else
+      {
+         _currentSpeed = speed;
+      }
+
       if(Input.GetKey(KeyCode.LeftArrow))
-         rb.velocity = new Vector2(-speed, rb.velocity.y);
+      {
+         rb.velocity = new Vector2(-_currentSpeed, rb.velocity.y);
+      }
       else if(Input.GetKey(KeyCode.RightArrow))
-         rb.velocity = new Vector2(speed, rb.velocity.y);
-      
+      {
+         rb.velocity = new Vector2(_currentSpeed, rb.velocity.y);
+      }
+
       if(rb.velocity.x > 0)
+      {
          rb.velocity += friction;
+      }
       else if(rb.velocity.x < 0)
+      {
          rb.velocity -= friction;
+      }
    }
 
    void HandleJump()
