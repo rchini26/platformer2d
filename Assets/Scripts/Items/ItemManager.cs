4b39cef953ade5698f914deb54ae8ac2712ed0ad
rchini26 <rafa.montm@gmail.com>
2025-10-19 16:56:05 -0300

    Added Coin Counter to UI

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Core.Singleton;
+using TMPro;
 
 public class ItemManager : Singleton<ItemManager>
 {
     public int coins;
+    public TextMeshProUGUI coinText;
     void Start()
     {
         Reset();
@@ -18,5 +20,11 @@ public class ItemManager : Singleton<ItemManager>
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        coinText.text = coins.ToString();
     }
 }
