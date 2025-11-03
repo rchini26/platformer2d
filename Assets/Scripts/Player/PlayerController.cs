86f1eabab2b8985fcb0e66c71ffdf9ade35b62f3
rchini26 <rafa.montm@gmail.com>
2025-11-03 11:33:43 -0300

    Tagged Enemies to Allow Jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -39,7 +39,7 @@ public class PlayerController : MonoBehaviour
 
    private void OnCollisionEnter2D(Collision2D collision)
    {
-      if (collision.gameObject.CompareTag("Ground"))
+      if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Enemy"))
       {
          isOnGround = true;
       }
